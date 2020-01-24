a793ec790d345f5bb790492101822a2b5ba1d109
Roberto Rivera <bobby@roberto-rivera.com>
2020-01-24 01:31:45 -0500

    Update to pdf and fixed padding issues

--- scripts/bobbyport.js
+++ scripts/bobbyport.js
@@ -1,12 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
-
-
     // Get all "navbar-burger" elements
     const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
 
     // Check if there are any navbar burgers
-    if ($navbarBurgers.length > 0) {
 
+    if ($navbarBurgers.length > 0) {
         // Add a click event on each of them
         $navbarBurgers.forEach( el => {
             el.addEventListener('click', () => {
@@ -22,5 +20,4 @@ document.addEventListener('DOMContentLoaded', () => {
             });
         });
     }
-
 });
